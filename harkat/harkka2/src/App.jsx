56507ffe53ecaed4c4133720a969a216a7f448a3
Henrik Heinonen <hohein@utu.fi>
2023-11-20 15:54:19 +0200

    h2 tbc

--- harkat/harkka2/src/App.jsx
+++ harkat/harkka2/src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from 'react'
 
+/* part c */
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
 const App = () => {
   
   const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
   /*
   setTimeout(
   () => setCounter(counter + 1),
@@ -10,20 +20,71 @@ const App = () => {
 
   console.log('rendering...', counter)
 */
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
-  const handleClick = () => {
-    console.log('clicked')
+
+/* part c */
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
   }
-    
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+  
+    const setToZero = () => {
+      console.log('resetting to zero, value before', counter)
+      setCounter(0)
+      setLeft(0)
+      setRight(0)
+    }
+  /* part d */
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+
+  const handleLeftClick = () =>{ 
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
   return (
+    
       <div>
-        <div>{counter}</div>
-        <button onClick={increaseByOne}>+</button>
-        <button onClick={decreaseByOne}>-</button>
-        <button onClick={setToZero}>zero</button>
+        <div>
+          <p>/* part c */</p>
+        <Display counter = {counter}/>
+        <Button handleClick={increaseByOne}
+          text="+"/>
+        <Button handleClick={decreaseByOne}
+        text = "-"/>
+        <Button handleClick={setToZero}
+        text = "zero"/>
+        </div>
+        <div>
+        <p>/* part d */</p>
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {right}
+        <p>{allClicks.join(' ')}</p>
+          
+
+
+
+
+
+
+
+        </div>
       </div>
+    
+    
   )
 
 }
