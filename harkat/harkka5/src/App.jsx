53bb4d99d05154aaff28122a47470cf1b416108d
Henrik Heinonen <hohein@utu.fi>
2023-11-25 02:40:08 +0200

    ad

--- harkat/harkka5/src/App.jsx
+++ harkat/harkka5/src/App.jsx
@@ -4,6 +4,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 
 const App = () => {
@@ -21,7 +22,7 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
   
   /* notet 'json servulta' promisella */
-  useEffect(() => {
+  /*useEffect(() => {
     axios
       .get('http://localhost:3001/notes')
       .then(response => {
@@ -29,6 +30,16 @@ const App = () => {
       })
   }, [])
   console.log('render', notes.length, 'notes')
+  */
+
+ /*käytetään tehtyä noteserviceä */
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
 
 
   const handleNoteChange = (event) => {
@@ -44,14 +55,52 @@ const App = () => {
       important: Math.random() > 0.5,
       id: notes.length + 1,
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+    // axios
+    //   .post('http://localhost:3001/notes', noteObject)
+    //   .then(response => {
+    //     setNotes(notes.concat(response.data))
+    //     setNewNote('')
+    //   })
   }
 
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
   
+  const toggleImportanceOf = (id) => {
+    /* määrittelee jokaiselle muistiinpanolle id-kenttään perustuvan yksilöivän urlin*/
+    const url = `http://localhost:3001/notes/${id}`
+    /* taulukon find metodilla etsitään halutun id:n omaava note*/
+    const note = notes.find(n => n.id === id) 
+    /* tehdään kopio oliolle "...note", jolle asetetaan important kenttä päinvastaiseksi '!'*/
+    const changedNote = { ...note, important: !note.important} 
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(n => n.id !== id ? n : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note ${note.content} was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      }) 
+    // axios
+    // /*uusi note lähetetään PUT pyynnön mukana palvelimelle */
+    //   .put(url, changedNote).then(response => {
+    //     /* takaisinkutsufunktiossa asetetaan komponentin App tilaan notes kaikki vanhat notet
+    //       paitsi muuttunut, josta tilaan asetetaan palvelimen palauttama versio */
+    //     setNotes(notes.map(n => n.id !== id ? n : response.data))
+    //   })
+  
+  }
   /*form*/
   /* lomakkeen input-komponentille on nyt
   rekisteröity tapahtumankäsittelijä
@@ -67,7 +116,10 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key = {note.id} note = {note} />
+          <Note
+            key = {note.id}
+            note = {note}
+            toggleImportance={() => toggleImportanceOf(note.id)} />
         )}
       </ul>
       <form onSubmit={addNote}>
